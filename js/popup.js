384a23e04249e1689cccb73c3f951d25981a5046
ThuongChu <chuthuong275@gmail.com>
2024-09-18 15:50:59 +0700

    add count number Invite

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,43 @@
 document.getElementById('start').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: () => {
+       // Click Invite buttons
+        function clickInviteButtons() {
           let spans = document.querySelectorAll('span');
+          let inviteCount = 0;
+
           spans.forEach(span => {
-            if (span.innerText === 'Invite') {
+            if (span.innerText.trim() === 'Invite') {
               span.click();
+              inviteCount++;
             }
           });
+
+          // Show message
+          if (inviteCount > 0) {
+            alert(`Was click ${inviteCount} button "Invite"`);
+          } else {
+            alert('not found "Invite".');
+          }
         }
-      });
+
+        function attemptClick() {
+          clickInviteButtons();
+
+          // Scroll and click Invite buttons again
+          let scrollInterval = setInterval(() => {
+            window.scrollBy(0, 1000);
+            if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+              clearInterval(scrollInterval);
+              clickInviteButtons(); // Thực hiện nhấp lại sau khi cuộn xong
+            }
+          }, 500);
+        }
+
+        attemptClick();
+      }
     });
   });
-  
+});
